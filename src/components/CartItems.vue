2ae5329cba9fb8f019d1acaf914ac2ebcef98ad6
Hayci <nataliaveles061@gmail.com>
2022-02-22 10:56:24 -0600

    ListProduct

---
+++ src/components/CartItems.vue
@@ -0,0 +1,15 @@
+<template>
+   <div id="root">
+     <ul v-for="(item, index) in cartItems" v:bind:key="index">
+      <li>{{item.productName}} {{item.quantity}}</li> 
+     </ul>
+   </div>
+</template>
+
+<script>
+export default{
+    name:'CartItems',
+    props:['cartItems','itemCount'],
+  
+}
+</script>
